545f7f871110d0632a77fe801d9c105af5912a53
Machado-SI <223562434+Machado-SI@users.noreply.github.com>
2025-09-08 18:19:32 -0300

    Criar rota de registro

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,16 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { db } from './db.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import yup from 'yup';
+
+// 
+const registerValidate = yup.object().shape({
+    nome: yup.string().required().min(5),
+    email: yup.string().email().required(),
+    senha: yup.string().required().min(7).max(32).matches(/[a-z]/).matches(/[A-Z]/).matches(/[0-9]/).matches(/[^a-zA-Z0-9]/)
+})
 
 // Configura dotenv
 dotenv.config();
@@ -56,6 +66,44 @@ app.get('/agendamentos', async (req, res) => {
     } catch (error) {
         console.error('Erro ao buscar agendamentos:', error);
         res.status(500).json({ error: 'Erro ao buscar agendamentos' });
+    }
+})
+
+// Rota para registrar um novo usuário
+app.post('/register', async (req, res) => {
+    try {
+        const {nome, senha, email} = req.body;
+
+        // Validação dos dados com yup
+        await registerValidate.validate({nome, senha, email}, {
+            abortEarly: false
+        });
+
+        // Verifica se o email já está em uso
+        const existingUser = await db.oneOrNone('SELECT id FROM users WHERE email = $1', [email]);
+        if (existingUser) {
+            return res.status(400).json({ error: 'Email já está em uso' });
+        }
+
+        // Hash da senha e inserção do novo usuário no banco de dados
+        const hashedPassword = await bcrypt.hash(senha, 10);
+        const newUser = await db.one(
+            'INSERT INTO usuarios (nome, email ,senha) VALUES ($1, $2, $3) RETURNING id, nome',
+            [nome, email, hashedPassword]
+        );
+        res.status(201).json(newUser);
+    } catch (error) {
+
+        // Tratamento de erros de validação do yup
+        if (error.name === 'ValidationError') {
+            console.error('Erro de validação:', error.errors);
+            return res.status(400).json({
+                message: 'Erro de validação',
+                errors: error.errors
+            })
+        }
+        console.error('Erro ao registrar usuário:', error);
+        res.status(500).json({ error: 'Erro ao registrar usuário' });
     }
 })
 
