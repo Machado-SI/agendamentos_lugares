3dcff6d961eee415cbb5b6c3aeebd1f876cc7852
Machado-SI <223562434+Machado-SI@users.noreply.github.com>
2025-09-10 19:45:42 -0300

    Criar rota e página de login

--- frontend/vite-project/src/Routers.jsx
+++ frontend/vite-project/src/Routers.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App.jsx';
-import Register from './components/Register.jsx';
+import Register from './Register.jsx';
+import Login from './login.jsx';
 
 function Routers() {
     return ( 
@@ -8,6 +9,7 @@ function Routers() {
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
         </BrowserRouter>
      );
