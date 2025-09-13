0ed94f17120d7d4270e536df297508637c144815
Machado-SI <223562434+Machado-SI@users.noreply.github.com>
2025-09-13 12:13:10 -0300

    feat(ui): implementa verificação de locais indisponíveis e desabilita botão de agendamento quando ocupado

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 function App() {
   const [locais, setLocaisList] = useState([])
@@ -17,29 +17,31 @@ function App() {
     setErrorMessage('')
     try {
       if (LocalSelecionado && dataHoraInicio && dataHoraFim) {
-      const novoAgendamento = {
-        local: LocalSelecionado.local,
-        dataHoraInicio,
-        dataHoraFim,
-      }
+        const novoAgendamento = {
+          local: LocalSelecionado.local,
+          dataHoraInicio,
+          dataHoraFim
+        }
 
-      const response = await fetch(`${API}/agendar`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(novoAgendamento)
-      })
-      if (!response.ok) {
-        const errorData = await response.json()
-        throw new Error(`${errorData.error}`)
-      }
-      setMensagem('Agendamento realizado com sucesso!')
-      buscaAgendamentos()
+        const response = await fetch(`${API}/agendar`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            // Adicionar token no cabeçalho Authorization para autenticação para a rota protegida
+            'Authorization': `Bearer ${token}`
+          },
+          body: JSON.stringify(novoAgendamento)
+        })
+        if (!response.ok) {
+          const errorData = await response.json()
+          throw new Error(`${errorData.error}`)
+        }
+        setMensagem('Agendamento realizado com sucesso!')
+        buscaAgendamentos()
 
-      setDataHoraInicio('')
-      setDataHoraFim('')
-    }
+        setDataHoraInicio('')
+        setDataHoraFim('')
+      }
     } catch (error) {
       console.error('Erro ao agendar local:', error.message)
       setErrorMessage(error.message)
@@ -62,9 +64,9 @@ function App() {
           throw new Error(`HTTP ${response.status} - ${response.statusText}`)
         }
       }
-      setAgendamentos(prevAgendamentos => 
-      prevAgendamentos.filter(agendamento => agendamento.id !== id)
-    )
+      setAgendamentos(prevAgendamentos =>
+        prevAgendamentos.filter(agendamento => agendamento.id !== id)
+      )
       setMensagem('Agendamento deletado com sucesso!')
     } catch (error) {
       setErrorMessage('Erro ao deletar agendamento: ' + error.message)
@@ -91,8 +93,8 @@ function App() {
     setErrorMessage('')
     setMensagem('')
     try {
-      const response =  await fetch(`${API}/lugares`)
-      if(!response.ok) {
+      const response = await fetch(`${API}/lugares`)
+      if (!response.ok) {
         throw new Error(`Erro ao buscar locais: ${response.status}`)
       }
       const data = await response.json()
@@ -108,7 +110,15 @@ function App() {
     buscaLocais()
   }, [])
 
-  return (  
+  // Pegar token do localStorage e decodificar para obter nome do usuário
+  const token = localStorage.getItem('token')
+  const usuario = token ? JSON.parse(atob(token.split('.')[1])) : null
+  const nomeUsuario = usuario ? usuario.nome : 'Usuário não identificado'
+
+  // Verificar se o local selecionado já está agendado
+  const localAgendado = agendamentos.some((agendamento) => agendamento.local === LocalSelecionado?.local)
+
+  return (
 
     <div className='min-h-screen bg-gray-50'>
       {/* Cabeçalho */}
@@ -126,21 +136,28 @@ function App() {
             <div className='bg-white p-6 rounded-lg shadow-md'>
               <h2 className='font-semibold text-[1.5rem]'>Locais Disponíveis</h2>
               <div className='space-y-4 mt-4'>
-              {locais.map((local) => (
-               <div  key={local.id_local} className={`border-2 rounded-lg p-4 cursor-pointer ${LocalSelecionado?.id_local === local.id_local ? 'border-blue-500 bg-blue-50' : 'border-gray-200'}`} onClick={() => setLocalSelecionado(local)}>
-                  <div className='flex justify-between'>
-                    <h3 className='font-semibold text-lg'>{local.local}</h3>
-                    <span className='bg-green-300 py-1 px-2 rounded-full'>Ativo</span>
+                {locais.map((local) => (
+                  <div key={local.id_local} className={`border-2 rounded-lg p-4 cursor-pointer ${LocalSelecionado?.id_local === local.id_local ? 'border-blue-500 bg-blue-50' : 'border-gray-200'}`} onClick={() => setLocalSelecionado(local)}>
+                    <div className='flex justify-between'>
+                      <h3 className='font-semibold text-lg'>{local.local}</h3>
+
+                      {/* Caso o lugar esteja agendado em algum horário, ele aparecerá como indisponível */}
+                      {local.local == agendamentos.map((agendamento) => {
+                        return agendamento.local
+                      }) ? <span className='bg-red-300 py-1 px-2 rounded-full'>Indisponível</span> : (
+                        <span className='bg-green-300 py-1 px-2 rounded-full'>Ativo</span>
+                      )}
+
+                    </div>
+                    <div className='my-3'>
+                      <p>{local.descricao}</p>
+                    </div>
+                    <div className='flex items-center gap-2'>
+                      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#b3b3b3"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z" /></svg>
+                      <p>{`Capacidade: ${local.capacidade} pessoas`}</p>
+                    </div>
                   </div>
-                  <div className='my-3'>
-                    <p>{local.descricao}</p>
-                  </div>
-                  <div className='flex items-center gap-2'>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#b3b3b3"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/></svg>
-                    <p>{`Capacidade: ${local.capacidade} pessoas`}</p>
-                  </div>
-               </div> 
-              ))}
+                ))}
               </div>
             </div>
           </div>
@@ -153,16 +170,24 @@ function App() {
               {LocalSelecionado ? (
                 <form onSubmit={handleAgendar}>
                   <div className='space-y-4'>
-                    <div> 
+                    <div>
                       <label className='block text-sm font-medium'>Data e Hora de Início</label>
-                      <input type="datetime-local" value={dataHoraInicio} onChange={(e) => setDataHoraInicio(e.target.value)} className='border-2 border-gray-300 focus:outline-none transition ease-in-out duration-300 focus:border-blue-500 py-2 px-3 rounded-lg w-full mt-[2px]' required/>
+                      <input type="datetime-local" value={dataHoraInicio} onChange={(e) => setDataHoraInicio(e.target.value)} className='border-2 border-gray-300 focus:outline-none transition ease-in-out duration-300 focus:border-blue-500 py-2 px-3 rounded-lg w-full mt-[2px]' required />
                     </div>
 
-                    <div> 
+                    <div>
                       <label className='block text-sm font-medium'>Data e Hora de Término</label>
-                      <input type="datetime-local" value={dataHoraFim} onChange={(e) => setDataHoraFim(e.target.value)} className='border-2 border-gray-300 focus:outline-none transition ease-in-out duration-300 focus:border-blue-500 py-2 px-3 rounded-lg w-full mt-[2px]' required/>
+                      <input type="datetime-local" value={dataHoraFim} onChange={(e) => setDataHoraFim(e.target.value)} className='border-2 border-gray-300 focus:outline-none transition ease-in-out duration-300 focus:border-blue-500 py-2 px-3 rounded-lg w-full mt-[2px]' required />
                     </div>
-                    <button type='submit' className='w-full bg-blue-600 py-2 px-4 rounded-lg text-white hover:bg-blue-700 hover:cursor-pointer transition ease-in-out duration-300'>Confirmar Agendamento</button>
+
+                    {/* Botão de agendar, desabilitado caso o local já esteja agendado */}
+                    {localAgendado ? (
+                      <button type='submit' disabled className='w-full bg-red-600 py-2 px-4 rounded-lg text-white hover:bg-red-700 transition ease-in-out duration-300 cursor-not-allowed'>Local já agendado</button>)
+                      : (
+                      <button type='submit' className='w-full bg-blue-600 py-2 px-4 rounded-lg text-white hover:bg-blue-700 hover:cursor-pointer transition ease-in-out duration-300'>Confirmar Agendamento</button>
+                      )
+                    }
+                    
                     {errorMessage && (
                       <p className='font-semibold text-red-500 text-md text-center'>{errorMessage}</p>
                     )}
@@ -185,6 +210,7 @@ function App() {
                   {agendamentos.map((agendamento) => (
                     <div key={agendamento.id} className='border-2 border-gray-200 rounded-lg p-4'>
                       <h3 className='font-semibold text-lg'>{agendamento.local}</h3>
+                      <p className='font-semibold text-gray-600'>{nomeUsuario}</p>
                       <p className='text-sm text-gray-600'>Início: {new Date(agendamento.data_inicio).toLocaleString()}</p>
                       <p className='text-sm text-gray-600'>Término: {new Date(agendamento.data_termino).toLocaleString()}</p>
                       <button className='cursor-pointer px-2 py-1 rounded-lg bg-red-600 text-white mt-2' onClick={() => deletar(agendamento.id)}>Excluir</button>
