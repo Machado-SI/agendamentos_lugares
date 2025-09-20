60a029f52d0749bd348b503c8545e97a5aaec1ff
Machado-SI <223562434+Machado-SI@users.noreply.github.com>
2025-09-20 12:31:34 -0300

    feat: fazer comentários para melhor entendimento

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -7,8 +7,13 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
+      // Proxy para redirecionar requisições da API para o backend durante o desenvolvimento.
       '/lugares': {
+        // Endereço do servidor backend onde a API está rodando.
         target: 'http://localhost:8888',
+
+        // Permite que o servidor de destino veja a requisição como se viesse do próprio servidor,
+        // útil para evitar problemas de CORS durante o desenvolvimento.
         changeOrigin: true,
       }
     }
